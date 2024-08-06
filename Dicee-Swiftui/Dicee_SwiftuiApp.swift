9520ef509db5d57138081d75c7b3bd090429fc70
“Brian <brian.anggriawan@komunal.co.id>
2024-08-06 18:23:18 +0700

    Initial Commit

---
+++ Dicee-Swiftui/Dicee_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftuiApp.swift
+//  Dicee-Swiftui
+//
+//  Created by Brian Anggriawan on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
